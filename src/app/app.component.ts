6bb0f4b01472c4be4688b52caeb98f0b5a244545
muskan shaikh <muskan@pixel6.co>
2021-05-06 14:24:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent implements OnInit {
     
   }
   slides = [
-    {'image': "../../assets/images/banner1.jpg"}, 
-    {'image': "../../assets/images/banner2.jpg"}, 
-    {'image': "../../assets/images/banner3.jpg"}, 
-    {'image': "../../assets/images/banner4.jpg"}, 
+    {'image': "assets/images/banner1.jpg"}, 
+    {'image': "assets/images/banner2.jpg"}, 
+    {'image': "assets/images/banner3.jpg"}, 
+    {'image': "assets/images/banner4.jpg"}, 
 
   ];
 
